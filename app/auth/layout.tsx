c6336c5edf532002ae532d72b6151582c0f38c50
Mohamad Salimi <contact@salimi.my>
2024-01-05 23:49:31 +0800

    Update auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,8 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      <nav>This is auth navbar</nav>
+    <div className='h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
       {children}
     </div>
   );
