7b9f71110f38f7a261789c11484e229dcb3a8360
Mohamad Salimi <contact@salimi.my>
2024-01-05 23:45:49 +0800

    Update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <p className='font-semibold'>NextAuth Starter</p>;
+import { Poppins } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import SignInButton from '@/components/auth/sign-in-button';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+});
+
+export default function HomePage() {
+  return (
+    <main className='flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+      <div className='space-y-6 text-center'>
+        <h1
+          className={cn(
+            'text-6xl font-semibold text-white drop-shadow-md',
+            font.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className='text-white text-lg'>A simple authentication service</p>
+        <div>
+          <SignInButton asChild>
+            <Button variant='secondary' size='lg'>
+              Sign in
+            </Button>
+          </SignInButton>
+        </div>
+      </div>
+    </main>
+  );
 }
