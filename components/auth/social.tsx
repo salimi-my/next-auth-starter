a6d6cc5d0beb9e675134bc20272df934419a3429
Mohamad Salimi <contact@salimi.my>
2024-01-06 21:11:29 +0800

    Create social component

---
+++ components/auth/social.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
+import { Button } from '@/components/ui/button';
+
+export function Social() {
+  const onClick = (provider: 'google' | 'github') => {};
+
+  return (
+    <div className='flex items-center w-full gap-x-2'>
+      <Button
+        size='lg'
+        className='w-full'
+        variant='outline'
+        onClick={() => onClick('google')}
+      >
+        <FcGoogle className='h-5 w-5' />
+      </Button>
+      <Button
+        size='lg'
+        className='w-full'
+        variant='outline'
+        onClick={() => onClick('github')}
+      >
+        <FaGithub className='h-5 w-5' />
+      </Button>
+    </div>
+  );
+}
