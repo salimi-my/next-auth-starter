8749fb626e57db7249f494d3a6d0464eb37c9899
Mohamad Salimi <contact@salimi.my>
2024-01-08 22:59:34 +0800

    Update sign up server action

--- actions/sign-up.ts
+++ actions/sign-up.ts
@@ -1,15 +1,38 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcryptjs';
 
 import { SignUpSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export async function signUp(values: z.infer<typeof SignUpSchema>) {
   const validatedFields = SignUpSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: 'Invalid fields!' };
+    return { error: 'Invalid fields.' };
   }
 
-  return { success: 'Email sent!' };
+  const { email, password, name } = validatedFields.data;
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'Email already exist.' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+
+  return { success: 'User created.' };
 }
