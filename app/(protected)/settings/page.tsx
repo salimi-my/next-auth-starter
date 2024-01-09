171d8972acdf5cfceac65e4dd094fb097197c6b5
Mohamad Salimi <contact@salimi.my>
2024-01-09 23:19:54 +0800

    Add sign out button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,20 @@
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
 
 export default async function SettingsPage() {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          'use server';
+
+          await signOut();
+        }}
+      >
+        <button type='submit'>Sign out</button>
+      </form>
+    </div>
+  );
 }
