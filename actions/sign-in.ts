cdf32d60150da09d6e15693bb9b05f8168a1790c
Mohamad Salimi <contact@salimi.my>
2024-01-09 23:15:04 +0800

    Update sign in server action

--- actions/sign-in.ts
+++ actions/sign-in.ts
@@ -1,15 +1,37 @@
 'use server';
 
 import * as z from 'zod';
+import { AuthError } from 'next-auth';
 
 import { SignInSchema } from '@/schemas';
+import { signIn as authSignIn } from '@/auth';
+import { DEFAULT_SIGNIN_REDIRECT } from '@/routes';
 
 export async function signIn(values: z.infer<typeof SignInSchema>) {
   const validatedFields = SignInSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: 'Invalid fields!' };
+    return { error: 'Invalid fields.' };
   }
 
-  return { success: 'Email sent!' };
+  const { email, password, code } = validatedFields.data;
+
+  try {
+    await authSignIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_SIGNIN_REDIRECT
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Incorrect email or password.' };
+        default:
+          return { error: 'Oops! Something went wrong.' };
+      }
+    }
+
+    throw error;
+  }
 }
