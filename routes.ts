39d2e4e1c4894a52b9413161d35b7aa03f3eb2c7
Mohamad Salimi <contact@salimi.my>
2024-01-09 23:14:34 +0800

    Create routes.ts

---
+++ routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes does not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ['/'];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect signed in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ['/auth/sign-in', '/auth/sign-up'];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * The default redirect path after logging in
+ * @type {string}
+ */
+export const DEFAULT_SIGNIN_REDIRECT = '/settings';
