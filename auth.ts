3b7b2ae5c9911edffd2cb534647a925dca9cda13
Mohamad Salimi <contact@salimi.my>
2024-01-10 23:10:32 +0800

    Add linkAccount event

--- auth.ts
+++ auth.ts
@@ -12,6 +12,18 @@ export const {
   signIn,
   signOut
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id
+        },
+        data: {
+          emailVerified: new Date()
+        }
+      });
+    }
+  },
   callbacks: {
     // async signIn({ user }) {
     //   const existingUser = await getUserById(user.id);
