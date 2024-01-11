aaf828a7044f74995d955c66d225c3b9c5b7f654
Mohamad Salimi <contact@salimi.my>
2024-01-11 23:22:52 +0800

    Create tokens lib

---
+++ lib/tokens.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { db } from '@/lib/db';
+import { getVerificationTokenByEmail } from '@/data/verification-token';
+
+export async function generateVerificationToken(email: string) {
+  const token = uuidv4();
+
+  // expire the token in one hour
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getVerificationTokenByEmail(email);
+
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: {
+        id: existingToken.id
+      }
+    });
+  }
+
+  const verficationToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  });
+
+  return verficationToken;
+}
