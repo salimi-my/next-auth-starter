e951a197847b093ce338291f65c8a20628eb0e61
Mohamad Salimi <contact@salimi.my>
2024-01-11 23:22:33 +0800

    Create verification token data

---
+++ data/verification-token.ts
@@ -0,0 +1,29 @@
+import { db } from '@/lib/db';
+
+export async function getVerificationTokenByToken(token: string) {
+  try {
+    const verificationToken = await db.verificationToken.findUnique({
+      where: {
+        token
+      }
+    });
+
+    return verificationToken;
+  } catch {
+    return null;
+  }
+}
+
+export async function getVerificationTokenByEmail(email: string) {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: {
+        email
+      }
+    });
+
+    return verificationToken;
+  } catch {
+    return null;
+  }
+}
