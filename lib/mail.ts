ec32dad4713ccef10671f3a25082e5cefc128c12
Mohamad Salimi <contact@salimi.my>
2024-01-12 23:20:38 +0800

    Update confirm link page

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const domain = process.env.APP_URL;
 
 export async function sendVerificationEmail(email: string, token: string) {
-  const confirmLink = `${domain}/auth/new-verification?token=${token}`;
+  const confirmLink = `${domain}/auth/email-verification?token=${token}`;
 
   await resend.emails.send({
     from: 'Next Auth Starter <confirmation@auth.salimi.my>',
