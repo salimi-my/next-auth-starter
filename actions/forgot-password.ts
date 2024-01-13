35dfcf23ba61d36605609164999a1325a840ea2c
Mohamad Salimi <contact@salimi.my>
2024-01-13 17:59:27 +0800

    Create forgot password server action

---
+++ actions/forgot-password.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import * as z from 'zod';
+
+import { getUserByEmail } from '@/data/user';
+import { ForgotPasswordSchema } from '@/schemas';
+import { sendPasswordResetEmail } from '@/lib/mail';
+import { generatePasswordResetToken } from '@/lib/tokens';
+
+export async function forgotPassword(
+  values: z.infer<typeof ForgotPasswordSchema>
+) {
+  const validatedFields = ForgotPasswordSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: 'Invalid email.' };
+  }
+
+  const { email } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    return { error: 'Email does not exist.' };
+  }
+
+  const passwordResetToken = await generatePasswordResetToken(email);
+  await sendPasswordResetEmail(
+    passwordResetToken.email,
+    passwordResetToken.token
+  );
+
+  return { success: 'Reset link sent to your email.' };
+}
