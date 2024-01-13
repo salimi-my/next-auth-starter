95a2e13e64bcc8a443b982735e0ca2e4789bfc0b
Mohamad Salimi <contact@salimi.my>
2024-01-13 21:58:48 +0800

    Create generate two factor token function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,8 +1,10 @@
+import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 
 import { db } from '@/lib/db';
 import { getVerificationTokenByEmail } from '@/data/verification-token';
 import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
 
 export async function generateVerificationToken(email: string) {
   const token = uuidv4();
@@ -56,4 +58,31 @@ export async function generatePasswordResetToken(email: string) {
   });
 
   return passwordResetToken;
+}
+
+export async function generateTwoFactorToken(email: string) {
+  const token = crypto.randomInt(100_000, 1_000_000).toString();
+
+  // Expire the token in one hour
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
+
+  const existingToken = await getTwoFactorTokenByEmail(email);
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: {
+        id: existingToken.id
+      }
+    });
+  }
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  });
+
+  return twoFactorToken;
 }
