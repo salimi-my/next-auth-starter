ea08252a0986ae39832db867e3147a2dfabb408b
Mohamad Salimi <contact@salimi.my>
2024-01-14 01:43:33 +0800

    Create sign out button component

---
+++ components/auth/sign-out-button.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { signOut } from '@/actions/sign-out';
+
+interface SignOutButtonProps {
+  children?: React.ReactNode;
+}
+
+export function SignOutButton({ children }: SignOutButtonProps) {
+  return (
+    <span onClick={() => signOut} className='cursor-pointer'>
+      {children}
+    </span>
+  );
+}
