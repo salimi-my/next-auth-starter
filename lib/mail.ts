7142d0eedc3b9270fcddc2a76724d2bc05c228fc
Mohamad Salimi <contact@salimi.my>
2024-01-13 17:58:41 +0800

    Create send password reset email

--- lib/mail.ts
+++ lib/mail.ts
@@ -14,3 +14,14 @@ export async function sendVerificationEmail(email: string, token: string) {
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
   });
 }
+
+export async function sendPasswordResetEmail(email: string, token: string) {
+  const resetLink = `${domain}/auth/reset-password?token=${token}`;
+
+  await resend.emails.send({
+    from: 'Next Auth Starter <reset@auth.salimi.my>',
+    to: email,
+    subject: 'Password Reset',
+    html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
+  });
+}
