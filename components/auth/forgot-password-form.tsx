c4c3f76223bd80cc01257e6333621c9c001dc881
Mohamad Salimi <contact@salimi.my>
2024-01-13 17:59:40 +0800

    Create forgot password form

---
+++ components/auth/forgot-password-form.tsx
@@ -0,0 +1,85 @@
+'use client';
+
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { useState, useTransition } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { ForgotPasswordSchema } from '@/schemas';
+import { FormError } from '@/components/form-error';
+import { FormSuccess } from '@/components/form-success';
+import { forgotPassword } from '@/actions/forgot-password';
+import { CardWrapper } from '@/components/auth/card-wrapper';
+
+export function ForgotPasswordForm() {
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof ForgotPasswordSchema>>({
+    resolver: zodResolver(ForgotPasswordSchema),
+    defaultValues: {
+      email: ''
+    }
+  });
+
+  const onSubmit = (values: z.infer<typeof ForgotPasswordSchema>) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      forgotPassword(values).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel='Forgot your password?'
+      backButtonLabel='Back to login'
+      backButtonHref='/auth/login'
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+          <div className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder='name@domain.com'
+                      type='email'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending} type='submit' className='w-full'>
+            Email password reset link
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
