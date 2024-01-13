5bd58be5b3be7abcd82c469ca1439d70e1ef424e
Mohamad Salimi <contact@salimi.my>
2024-01-13 20:36:28 +0800

    Create reset password form

---
+++ components/auth/reset-password-form.tsx
@@ -0,0 +1,107 @@
+'use client';
+
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { useState, useTransition } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { ResetPasswordSchema } from '@/schemas';
+import { FormError } from '@/components/form-error';
+import { FormSuccess } from '@/components/form-success';
+import { resetPassword } from '@/actions/reset-password';
+import { CardWrapper } from '@/components/auth/card-wrapper';
+
+export function ResetPasswordForm() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof ResetPasswordSchema>>({
+    resolver: zodResolver(ResetPasswordSchema),
+    defaultValues: {
+      password: ''
+    }
+  });
+
+  const onSubmit = (values: z.infer<typeof ResetPasswordSchema>) => {
+    setError('');
+    setSuccess('');
+
+    startTransition(() => {
+      resetPassword(values, token).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel='Enter a new password'
+      backButtonLabel='Back to sign in'
+      backButtonHref='/auth/sign-in'
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
+          <div className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder='••••••••'
+                      type='password'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='confirm'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder='••••••••'
+                      type='password'
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending} type='submit' className='w-full'>
+            Reset password
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
