59f292871dc304add2facb682c4c0de8f4cce621
Mohamad Salimi <contact@salimi.my>
2024-01-13 17:57:58 +0800

    Create generate reset token function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { db } from '@/lib/db';
 import { getVerificationTokenByEmail } from '@/data/verification-token';
+import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
 
 export async function generateVerificationToken(email: string) {
   const token = uuidv4();
@@ -28,4 +29,31 @@ export async function generateVerificationToken(email: string) {
   });
 
   return verficationToken;
+}
+
+export async function generatePasswordResetToken(email: string) {
+  const token = uuidv4();
+
+  // Expire the token in one hour
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id
+      }
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  });
+
+  return passwordResetToken;
 }
