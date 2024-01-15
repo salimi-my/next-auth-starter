f449dedcd33888347db08d97cf450f5575f3af42
Mohamad Salimi <contact@salimi.my>
2024-01-15 23:23:25 +0800

    Create admin page

---
+++ app/(protected)/admin/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { toast } from 'sonner';
+import { UserRole } from '@prisma/client';
+
+import { admin } from '@/actions/admin';
+import { Button } from '@/components/ui/button';
+import { RoleGate } from '@/components/auth/role-gate';
+import { FormSuccess } from '@/components/form-success';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+
+export default function AdminPage() {
+  const onApiRouteClick = () => {
+    fetch('/api/admin').then((response) => {
+      if (response.ok) {
+        toast.success('API route allowed.');
+      } else {
+        toast.error('API route forbidden.');
+      }
+    });
+  };
+
+  const onServerActionClick = () => {
+    admin().then((data) => {
+      if (data.error) {
+        toast.error(data.error);
+      }
+
+      if (data.success) {
+        toast.success(data.success);
+      }
+    });
+  };
+
+  return (
+    <Card className='w-[600px]'>
+      <CardHeader>
+        <p className='text-2xl font-semibold text-center'>🔑 Admin</p>
+      </CardHeader>
+      <CardContent className='space-y-4'>
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message='You are allowed to see this content!' />
+        </RoleGate>
+        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md'>
+          <p className='text-sm font-medium'>Admin-only API Route</p>
+          <Button onClick={onApiRouteClick}>Click to test</Button>
+        </div>
+
+        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-md'>
+          <p className='text-sm font-medium'>Admin-only Server Action</p>
+          <Button onClick={onServerActionClick}>Click to test</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
