299fa0360662d090d9f27f018a04813ee4352494
Mohamad Salimi <contact@salimi.my>
2024-01-16 18:09:40 +0800

    Create settings schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,5 @@
 import * as z from 'zod';
+import { UserRole } from '@prisma/client';
 
 export const SignInSchema = z.object({
   email: z.string().email({
@@ -38,13 +39,66 @@ export const ForgotPasswordSchema = z.object({
 
 export const ResetPasswordSchema = z
   .object({
-    password: z.string().min(6, {
+    password: z.string().min(8, {
       message: 'Minimum of 8 characters required.'
     }),
     confirm: z.string().min(8, {
       message: 'Minimum 8 characters required.'
     })
   })
+  .refine((data) => data.password === data.confirm, {
+    message: 'Passwords does not match.',
+    path: ['confirm']
+  });
+
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    currentPassword: z.optional(
+      z.string().min(8, {
+        message: 'Minimum of 8 characters required.'
+      })
+    ),
+    password: z.optional(
+      z.string().min(8, {
+        message: 'Minimum of 8 characters required.'
+      })
+    ),
+    confirm: z.optional(
+      z.string().min(8, {
+        message: 'Minimum 8 characters required.'
+      })
+    )
+  })
+  .refine(
+    (data) => {
+      if (data.currentPassword && !data.password) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: 'New password is required!',
+      path: ['password']
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.password && !data.currentPassword) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: 'Current password is required!',
+      path: ['currentPassword']
+    }
+  )
   .refine((data) => data.password === data.confirm, {
     message: 'Passwords does not match.',
     path: ['confirm']
