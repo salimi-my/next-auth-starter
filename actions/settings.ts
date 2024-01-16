9a081500508c9eea72137a5063d564a83620f4a6
Mohamad Salimi <contact@salimi.my>
2024-01-16 18:09:55 +0800

    Create settings server action

---
+++ actions/settings.ts
@@ -0,0 +1,94 @@
+'use server';
+
+import * as z from 'zod';
+import bcrypt from 'bcryptjs';
+
+import { db } from '@/lib/db';
+import { update } from '@/auth';
+import { SettingsSchema } from '@/schemas';
+import { sendVerificationEmail } from '@/lib/mail';
+import { currentUser } from '@/lib/authentication';
+import { generateVerificationToken } from '@/lib/tokens';
+import { getUserByEmail, getUserById } from '@/data/user';
+
+export async function settings(values: z.infer<typeof SettingsSchema>) {
+  const user = await currentUser();
+
+  if (!user) {
+    return { error: 'Unauthorized.' };
+  }
+
+  const dbUser = await getUserById(user.id);
+
+  if (!dbUser) {
+    return { error: 'Unauthorized.' };
+  }
+
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.currentPassword = undefined;
+    values.password = undefined;
+    values.confirm = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: 'Email already exist.' };
+    }
+
+    const verificationToken = await generateVerificationToken(values.email);
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: 'Confirmation email sent.' };
+  }
+
+  if (
+    values.currentPassword &&
+    values.password &&
+    values.confirm &&
+    dbUser.password
+  ) {
+    const passwordsMatch = await bcrypt.compare(
+      values.currentPassword,
+      dbUser.password
+    );
+
+    if (!passwordsMatch) {
+      return { error: 'Incorrect password.' };
+    }
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(values.password, salt);
+
+    values.password = hashedPassword;
+    values.currentPassword = undefined;
+    values.confirm = undefined;
+  }
+
+  const updatedUser = await db.user.update({
+    where: {
+      id: dbUser.id
+    },
+    data: {
+      ...values
+    }
+  });
+
+  update({
+    user: {
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isTwoFactorEnabled: updatedUser.isTwoFactorEnabled,
+      role: updatedUser.role
+    }
+  });
+
+  return { success: 'Settings updated.' };
+}
