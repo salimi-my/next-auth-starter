a6b5caa6de2eda6222fca764295058de81967c24
Mohamad Salimi <contact@salimi.my>
2024-01-18 18:38:56 +0800

    Update client page

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import { Computer } from 'lucide-react';
+
 import { UserInfo } from '@/components/user-info';
 import { useCurrentUser } from '@/hooks/use-current-user';
 
 export default function ClientPage() {
   const user = useCurrentUser();
 
-  return <UserInfo label='📱 Client component' user={user} />;
+  return <UserInfo icon={Computer} label='Client component' user={user} />;
 }
