ae095fd7fba4975e388cb7b71103a25064156f9a
Mohamad Salimi <contact@salimi.my>
2024-01-18 18:38:38 +0800

    Update user info component

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -1,51 +1,59 @@
+import { LucideIcon } from 'lucide-react';
+
 import { ExtendedUser } from '@/next-auth';
 import { Badge } from '@/components/ui/badge';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 
 interface UserInfoProps {
   user?: ExtendedUser;
   label: string;
+  icon: LucideIcon;
 }
 
-export function UserInfo({ user, label }: UserInfoProps) {
+export function UserInfo({ user, label, icon: Icon }: UserInfoProps) {
   return (
-    <Card className='w-[600px] shadow-md'>
-      <CardHeader>
-        <p className='text-2xl font-semibold text-center'>{label}</p>
-      </CardHeader>
-      <CardContent className='space-y-4'>
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
-          <p className='text-sm font-medium'>ID</p>
-          <p className='truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md'>
-            {user?.id}
-          </p>
-        </div>
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
-          <p className='text-sm font-medium'>Name</p>
-          <p className='truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md'>
-            {user?.name}
-          </p>
-        </div>
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
-          <p className='text-sm font-medium'>Email</p>
-          <p className='truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md'>
-            {user?.email}
-          </p>
-        </div>
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
-          <p className='text-sm font-medium'>Role</p>
-          <p className='truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md'>
-            {user?.role}
-          </p>
-        </div>
+    <div className='flex flex-col'>
+      <h2 className='text-3xl font-bold tracking-tight pb-4 flex items-center'>
+        <Icon className='mr-2' />
+        {label}
+      </h2>
+      <Card className='w-[600px]'>
+        <CardContent className='space-y-4 p-6'>
+          <div className='flex flex-row items-center justify-between rounded-md border px-3 py-1.5'>
+            <p className='text-sm font-medium'>ID</p>
+            <p className='truncate text-xs max-w-[200px] font-mono px-2 bg-zinc-100 rounded-sm'>
+              {user?.id}
+            </p>
+          </div>
+          <div className='flex flex-row items-center justify-between rounded-md border px-3 py-1.5'>
+            <p className='text-sm font-medium'>Name</p>
+            <p className='truncate text-xs max-w-[200px] font-mono px-2 bg-zinc-100 rounded-sm'>
+              {user?.name}
+            </p>
+          </div>
+          <div className='flex flex-row items-center justify-between rounded-md border px-3 py-1.5'>
+            <p className='text-sm font-medium'>Email</p>
+            <p className='truncate text-xs max-w-[200px] font-mono px-2 bg-zinc-100 rounded-sm'>
+              {user?.email}
+            </p>
+          </div>
+          <div className='flex flex-row items-center justify-between rounded-md border px-3 py-1.5'>
+            <p className='text-sm font-medium'>Role</p>
+            <p className='truncate text-xs max-w-[200px] font-mono px-2 bg-zinc-100 rounded-sm'>
+              {user?.role}
+            </p>
+          </div>
 
-        <div className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
-          <p className='text-sm font-medium'>Two Factor Authentication</p>
-          <Badge variant={user?.isTwoFactorEnabled ? 'success' : 'destructive'}>
-            {user?.isTwoFactorEnabled ? 'ON' : 'OFF'}
-          </Badge>
-        </div>
-      </CardContent>
-    </Card>
+          <div className='flex flex-row items-center justify-between rounded-md border px-3 py-1.5'>
+            <p className='text-sm font-medium'>Two Factor Authentication</p>
+            <Badge
+              variant={user?.isTwoFactorEnabled ? 'success' : 'destructive'}
+            >
+              {user?.isTwoFactorEnabled ? 'ON' : 'OFF'}
+            </Badge>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
