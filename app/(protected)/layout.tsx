08c8ab31854ad300ec4f077a928368472547223e
Mohamad Salimi <contact@salimi.my>
2024-01-18 18:09:12 +0800

    Update layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,9 +6,13 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
-      <Navbar />
-      {children}
+    <div className='h-full w-full flex flex-col gap-y-10 items-center justify-center'>
+      <div className='overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl'>
+        <div className='flex flex-col'>
+          <Navbar />
+          <div className='p-8'>{children}</div>
+        </div>
+      </div>
     </div>
   );
 }
