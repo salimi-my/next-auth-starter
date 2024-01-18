10ca2b469ed0f9c9acccc43ae50dfb0c91825118
Mohamad Salimi <contact@salimi.my>
2024-01-18 18:38:47 +0800

    Update server page

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,8 +1,10 @@
+import { Server } from 'lucide-react';
+
 import { UserInfo } from '@/components/user-info';
 import { currentUser } from '@/lib/authentication';
 
 export default async function ServerPage() {
   const user = await currentUser();
 
-  return <UserInfo label='💻 Server component' user={user} />;
+  return <UserInfo icon={Server} label='Server component' user={user} />;
 }
