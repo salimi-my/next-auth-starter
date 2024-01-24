8fccd4fb25be43163814718023aa525be3d822e3
Mohamad Salimi <contact@salimi.my>
2024-01-24 23:45:37 +0800

    Update form success

--- components/form-success.tsx
+++ components/form-success.tsx
@@ -8,8 +8,10 @@ export function FormSuccess({ message }: FormSuccessProps) {
   if (!message) return null;
 
   return (
-    <div className='bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-emerald-500'>
-      <CheckCircledIcon className='h-4 w-4' />
+    <div className='bg-emerald-500/15 p-3 rounded-md flex items-center space-x-2 text-xs md:text-sm text-emerald-500'>
+      <div className='w-4 h-4'>
+        <CheckCircledIcon className='h-4 w-4' />
+      </div>
       <p>{message}</p>
     </div>
   );
