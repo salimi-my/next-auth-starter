62928b4a34c9b0aa87a24c7c57e19b9777e218f8
Mohamad Salimi <contact@salimi.my>
2024-01-24 22:45:48 +0800

    Update auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='h-full min-h-screen flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
+    <div className='h-full min-h-screen flex items-center justify-center'>
       {children}
     </div>
   );
