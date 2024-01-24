bc2220924ac6a88392eb01eb8756e6927a9a58a3
Mohamad Salimi <contact@salimi.my>
2024-01-24 23:45:45 +0800

    Update form error

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -8,8 +8,10 @@ export function FormError({ message }: FormErrorProps) {
   if (!message) return null;
 
   return (
-    <div className='bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive'>
-      <ExclamationTriangleIcon className='h-4 w-4' />
+    <div className='bg-destructive/15 p-3 rounded-md flex items-center space-x-2 text-xs md:text-sm text-destructive'>
+      <div className='w-4 h-4'>
+        <ExclamationTriangleIcon className='h-4 w-4' />
+      </div>
       <p>{message}</p>
     </div>
   );
