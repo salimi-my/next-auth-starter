bd41f2fcb92c24bbbfdf6654d12f8f841ed3cf1a
Mohamad Salimi <contact@salimi.my>
2024-01-24 23:39:17 +0800

    Update protected layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -6,8 +6,8 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='h-full min-h-screen py-10 w-full flex flex-col gap-y-10 items-center justify-center'>
-      <div className='overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl'>
+    <div className='h-full min-h-screen py-10 px-4 w-full flex flex-col gap-y-10 items-center justify-center'>
+      <div className='w-full max-w-[600px] overflow-hidden rounded-[0.5rem] border bg-background shadow-md md:shadow-xl'>
         <div className='flex flex-col'>
           <Navbar />
           <div className='p-8'>{children}</div>
