e354aa43113abc94f34f6f6655f4d5f96e290e29
Mohamad Salimi <contact@salimi.my>
2024-01-25 23:45:55 +0800

    Update auth layout padding

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='h-full min-h-screen flex items-center justify-center'>
+    <div className='h-full min-h-screen flex items-center justify-center px-4 md:px-0'>
       {children}
     </div>
   );
